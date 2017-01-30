cf6446ddc3ab68a1856f9b4e150b098f402cc79a
andypagdin <andyjpagdin@gmail.com>
2017-01-30 21:59:34 +0000

    Trying out Yahoo Finance API
    Calls to the finance api returning market data

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,8 +9,23 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ["$firebaseObject", function($firebaseObject) {
-	const rootRef = firebase.database().ref().child('angular');
-	const ref = rootRef.child('object');
-	this.object = $firebaseObject(ref);
+// Fetching data from Firebase database
+// .controller('View1Ctrl', ["$firebaseObject", function($firebaseObject) {
+// 	const rootRef = firebase.database().ref().child('angular');
+// 	const ref = rootRef.child('object');
+// 	this.object = $firebaseObject(ref);
+// }]);
+
+.controller('View1Ctrl', ['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
+	var symbolOne = "AAPL";
+	var symbolTwo = "YHOO";
+	var tick = function(){
+		var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+symbolTwo+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+  		$http.get(url).then(function(response) {
+        	$scope.query = response.data.query.results.quote;
+        	$timeout(tick, 2000);
+        	//console.log(response.data.query.results.quote)
+    	});
+  	};
+  	tick();
 }]);
